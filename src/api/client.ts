3d6c162de632a477d03d3011f92392db163a2ec2
ladikusz <ladikszabolcs@draken.hu>
2024-11-25 14:48:07 +0100

    setting render.com url-s

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://unicourse-backend.onrender.com';
 
 export const apiClient = axios.create({
     baseURL: API_URL,
